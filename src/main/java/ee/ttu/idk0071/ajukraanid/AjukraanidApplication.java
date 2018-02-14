f081e128fd005b9812540af89d2ce67977781e38
Karl-Andero Mere <kamere@ttu.ee>
2018-02-14 08:55:31 +0200

    Spring Boot initialization

---
+++ src/main/java/ee/ttu/idk0071/ajukraanid/AjukraanidApplication.java
@@ -0,0 +1,12 @@
+package ee.ttu.idk0071.ajukraanid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjukraanidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjukraanidApplication.class, args);
+	}
+}
---
+++ src/test/java/ee/ttu/idk0071/ajukraanid/AjukraanidApplicationTests.java
@@ -0,0 +1,16 @@
+package ee.ttu.idk0071.ajukraanid;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AjukraanidApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
