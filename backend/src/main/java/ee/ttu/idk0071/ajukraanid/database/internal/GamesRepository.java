6c495039dc05cb43e6d435b98adb5d1a21fef374
nilill <nilill@ttu.ee>
2018-03-24 02:56:40 +0200

    This wont be merged just my branch with my files.

--- backend/src/main/java/ee/ttu/idk0071/ajukraanid/database/internal/GamesRepository.java
+++
@@ -1,6 +0,0 @@
-package ee.ttu.idk0071.ajukraanid.database.internal;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface GamesRepository extends CrudRepository<Games, Long> {
-}
--- backend/src/main/java/ee/ttu/idk0071/ajukraanid/database/internal/PlayersRepository.java
+++
@@ -1,6 +0,0 @@
-package ee.ttu.idk0071.ajukraanid.database.internal;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface PlayersRepository extends CrudRepository<Players, Long> {
-}
