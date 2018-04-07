532fdd9acd02374b960f61f5319d1e7989f64a04
Karl-Andero Mere <anddero@gmail.com>
2018-04-07 14:11:11 +0300

    Refactored some local variable names

--- backend/src/main/java/ee/ttu/idk0071/ajukraanid/controller/GameController.java
+++ backend/src/main/java/ee/ttu/idk0071/ajukraanid/controller/GameController.java
@@ -59,17 +59,17 @@ class GameController {
     }
 
     String joinGame(int gameCode, String playerName) {
-        Optional<Game> game = findActiveGame(gameCode);
+        Optional<Game> optionalGame = findActiveGame(gameCode);
 
-        if (!game.isPresent()) {
+        if (!optionalGame.isPresent()) {
             return createErrorResponse("Did not find such game with game code: " + gameCode); // TODO Exception
         }
 
-        if (findPlayer(game.get(), playerName).isPresent()) { // TODO Ignore case check
+        if (findPlayer(optionalGame.get(), playerName).isPresent()) { // TODO Ignore case check
             return createErrorResponse("Such username is already taken.");
         }
 
-        new Player(game.get(), playerName);
+        new Player(optionalGame.get(), playerName);
         return fetchState(gameCode);
     }
 
@@ -78,25 +78,25 @@ class GameController {
      * @return the game state, if it is in lobby then return the list of player names.
      */
     String startGame(int gameCode) {
-        Optional<Game> game = findActiveGame(gameCode);
+        Optional<Game> optionalGame = findActiveGame(gameCode);
 
-        if (!game.isPresent()) {
+        if (!optionalGame.isPresent()) {
             return createErrorResponse("Was not able to start the game because such game was not found.");
         }
 
-        executor.submit(new GameRunner(game.get()));
+        executor.submit(new GameRunner(optionalGame.get()));
         return fetchState(gameCode);
     }
 
     String fetchState(int gameCode) {
-        Optional<Game> optGame = findActiveGame(gameCode);
+        Optional<Game> optionalGame = findActiveGame(gameCode);
 
-        if (!optGame.isPresent()) {
+        if (!optionalGame.isPresent()) {
             return createErrorResponse("No game found with game code: " + gameCode);
         }
 
-        Game game = optGame.get();
-        Game.State gameState = optGame.get().getGameState();
+        Game game = optionalGame.get();
+        Game.State gameState = optionalGame.get().getGameState();
 
         Object data;
         switch (gameState) {
@@ -137,45 +137,45 @@ class GameController {
 
     String submitAnswer(int gameCode, String playerName, String answer) {
         // TODO Need Question Number
-        Optional<Game> game = findActiveGame(gameCode);
-        if (!game.isPresent()) {
+        Optional<Game> optionalGame = findActiveGame(gameCode);
+        if (!optionalGame.isPresent()) {
             return createErrorResponse("Did not find such game with game code: " + gameCode);
         }
-        Optional<Player> player = findPlayer(game.get(), playerName);
-        if (!player.isPresent()) {
+        Optional<Player> optionalPlayer = findPlayer(optionalGame.get(), playerName);
+        if (!optionalPlayer.isPresent()) {
             return createErrorResponse("Wrong username was given");
         }
 
-        Question currentQuestion = getCurrentQuestion(game.get());
+        Question currentQuestion = getCurrentQuestion(optionalGame.get());
 
         boolean hasAnswered = currentQuestion.getAnswers().stream()
                 .anyMatch(ans -> ans.getPlayer().getName().equals(playerName));
         if (hasAnswered) {
             return createErrorResponse("You already answered the question");
         }
-        new Answer(currentQuestion, player.get(), answer);
+        new Answer(currentQuestion, optionalPlayer.get(), answer);
         return createSuccessResponse("Your answer was submitted.");
     }
 
     String givePoints(int gameCode, String giverName, String targetName) {
         // TODO Need Question Number
-        Optional<Game> optGame = findActiveGame(gameCode);
+        Optional<Game> optionalGame = findActiveGame(gameCode);
 
-        if (!optGame.isPresent()) {
+        if (!optionalGame.isPresent()) {
             return createErrorResponse("Game with such id was not found");
         }
 
-        Game game = optGame.get();
+        Game game = optionalGame.get();
 
-        Optional<Player> optGiver = findPlayer(game, giverName);
-        Optional<Player> optTarget = findPlayer(game, targetName);
+        Optional<Player> optionalGiver = findPlayer(game, giverName);
+        Optional<Player> optionalTarget = findPlayer(game, targetName);
 
-        if (!optGiver.isPresent() || !optTarget.isPresent()) {
+        if (!optionalGiver.isPresent() || !optionalTarget.isPresent()) {
             return createErrorResponse("Wrong username was given");
         }
 
-        Player giver = optGiver.get();
-        Player target = optTarget.get();
+        Player giver = optionalGiver.get();
+        Player target = optionalTarget.get();
 
         Question currentQuestion = getCurrentQuestion(game);
         ArrayList<Evaluation> evaluations = currentQuestion.getEvaluations();
@@ -211,20 +211,20 @@ class GameController {
     }
 
     String removePlayer(int gameCode, String playerName) {
-        Optional<Game> optGame = findActiveGame(gameCode);
+        Optional<Game> optionalGame = findActiveGame(gameCode);
 
-        if (!optGame.isPresent()) {
+        if (!optionalGame.isPresent()) {
             return createErrorResponse("Did not find such game with game code: " + gameCode);
         }
 
-        Optional<Player> optPlayer = findPlayer(optGame.get(), playerName);
+        Optional<Player> optionalPlayer = findPlayer(optionalGame.get(), playerName);
 
-        if (!optPlayer.isPresent()) {
+        if (!optionalPlayer.isPresent()) {
             return createErrorResponse("Invalid player name: " + playerName);
         }
 
-        optGame.get().getPlayers().remove(optPlayer.get());
-        optPlayer.get().setValid(false);
+        optionalGame.get().getPlayers().remove(optionalPlayer.get());
+        optionalPlayer.get().setValid(false);
         return createSuccessResponse("Player successfully removed: " + playerName);
     }
 
