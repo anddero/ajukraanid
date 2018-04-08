f79faa2bbade57872adfc17616994b9e04fd7d2e
Karl-Andero Mere <anddero@gmail.com>
2018-04-08 20:17:06 +0300

    Timestamp added to log files on startup

--- backend/src/main/java/ee/ttu/idk0071/ajukraanid/AjukraanidApplication.java
+++ backend/src/main/java/ee/ttu/idk0071/ajukraanid/AjukraanidApplication.java
@@ -3,11 +3,17 @@ package ee.ttu.idk0071.ajukraanid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @SpringBootApplication
 public class AjukraanidApplication {
 
 	public static void main(String[] args) {
+	    System.setProperty("ajukraanid.util.start-time-stamp",
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss")));
 		System.setProperty("java.util.logging.manager", "org.apache.logging.log4j.jul.LogManager");
+
 		SpringApplication.run(AjukraanidApplication.class, args);
 	}
 }
